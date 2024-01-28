15137e95223b9e4b5d191ffef69e3dd9a26e08bc
Paul Patterson <paulpatterson@Pauls-MacBook-Pro.local>
2024-01-27 23:16:51 -0600

    First Commit defining types

---
+++ Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace HoneyRaesAPI.Models
+{
+	public class Employee
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Specialty { get; set; }
+	}
+}
+
